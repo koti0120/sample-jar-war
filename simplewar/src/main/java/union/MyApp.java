ba2094dbfd96161b81f21d5c6cd80049a5b9d21a
venkat <venkat@gmail.com>
2021-07-29 19:33:56 +0530

    developer develops jar war files

---
+++ simplewar/src/main/java/union/MyApp.java
@@ -0,0 +1,46 @@
+package union;
+
+import union.model.Person;
+import io.mateu.mdd.core.annotations.Action;
+import io.mateu.mdd.core.annotations.Caption;
+import io.mateu.mdd.core.app.AbstractAction;
+import io.mateu.mdd.core.app.MDDOpenCRUDAction;
+import io.mateu.mdd.core.app.SimpleMDDApplication;
+import io.mateu.mdd.core.util.Helper;
+import java.io.IOException;
+
+public class MyApp extends SimpleMDDApplication {
+
+    @Action(value = "Say hello", order = 1)
+    public String hello(String name) {
+        return "Hello " + name + "!";
+    }
+
+
+    @Action(order = 2)
+    public Class people() {
+        return Person.class;
+    }
+
+    @Action(order = 3)
+    public String md5(String s) {
+        return Helper.md5(s);
+    }
+
+    @Action(order = 4)
+    public String getThreadName() {
+        return Thread.currentThread().getName();
+    }
+
+    @Action(order = 5)
+    public String ls() throws Exception {
+        return Helper.toHtml(Helper.runCommand("ls -lh"));
+    }
+
+
+    @Action(order = 6)
+    public String df() throws Exception {
+        return Helper.toHtml(Helper.runCommand("df -h"));
+    }
+
+}
---
+++ simplewar/src/main/java/union/model/Gender.java
@@ -0,0 +1,7 @@
+package union.model;
+
+public enum Gender {
+
+    FEMALE, MALE
+
+}
